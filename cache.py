fde017acc65816f99319173ea6481646ddc73ced
ncread <noahcread@gmail.com>
2026-08-05 19:44:19 -0700

    update price ttl cache

--- cache.py
+++ cache.py
@@ -1,4 +1,4 @@
 from cachetools import TTLCache
 
 price_cache = TTLCache(maxsize=500, ttl=60*60*24) #24 hrs
-feature_cache = TTLCache(maxsize=500, ttl=60*60*2) #2 hrs
+feature_cache = TTLCache(maxsize=500, ttl=1800) #30 mins
